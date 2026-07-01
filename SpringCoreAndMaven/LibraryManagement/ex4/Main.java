de4e7163b999cca2d1633f9f1583a6d2aaabd145
23f2000299 <23f2000299@ds.study.iitm.ac.in>
2026-07-01 10:30:57 +0530

    ex4 - spring maven

---
+++ SpringCoreAndMaven/LibraryManagement/ex4/Main.java
@@ -0,0 +1,22 @@
+package com.cts.LibraryManagement;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class Main {
+
+    public static void main(String args[])
+    {
+
+        ApplicationContext context =
+        new ClassPathXmlApplicationContext(
+        "applicationContext.xml");
+
+
+        System.out.println("Spring Started");
+
+    }
+
+}
