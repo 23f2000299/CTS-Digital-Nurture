22dfbd89dc4e7f310403fd3176b0b81cb4b3718f
23f2000299 <23f2000299@ds.study.iitm.ac.in>
2026-07-02 08:50:23 +0530

    spring-rest handson ex1

---
+++ spring-rest-handson/simple-spring web project/src/test/java/com/springweb/spring_learn/SpringLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springweb.spring_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
